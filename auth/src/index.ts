7245b2003f6f48fceefe3f9291f0f8f619df6e99
Anju Alexander <Anju.Alexander1@ibm.com>
2025-03-31 16:25:53 +0530

    first commit

---
+++ auth/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import {json} from 'body-parser';
+
+// import { currentUser } from './routes/currentUser';
+// import { signinRouter } from './routes/signin';
+// import { signoutRouter } from './routes/signout';
+//import { signupUser } from './routes/signup';
+
+
+
+const app = express();
+app.use(json());
+// app.use(currentUser);
+// app.use(signinRouter);
+// app.use(signoutRouter);
+//app.use(signupUser);
+
+
+app.listen(3000, () =>{
+
+    console.log('Listening on port 3000!!');
+});
