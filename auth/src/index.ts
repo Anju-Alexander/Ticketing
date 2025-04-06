2d75d5dc8061155cdb79c60c8d12e7468bede067
Anju Alexander <75101813+Anju-Alexander@users.noreply.github.com>
2025-04-06 23:24:13 +0530

    password hashing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import {json} from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUser } from './routes/currentUser';
 import { signinRouter } from './routes/signin';
@@ -24,9 +25,20 @@ app.all('*',async(req,res,next)=>{
 app.use(errorHandler);
 
 
-
+const start = async () => {
+    try{
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        console.log('Connected to mongo db');
+    }
+    catch(err)
+    {
+        console.error(err);
+    }
+}
 
 app.listen(3000, () =>{
 
-    console.log('Listening on port  3000!!!!');
+    console.log('Hello Listening on port 3000!!!');
 });
+
+start();
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,6 +2,8 @@ import express, {Request, Response} from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { DatabaseConnectionError } from '../errors/database-connection-error';
+import {User} from '../models/user'
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -22,10 +24,17 @@ router.post('/api/users/signup', [
     }
 
     const {email, password } = req.body;
+    const existingUser = await User.findOne({email});
 
-   console.log('creating a user...');
-   throw new DatabaseConnectionError();
-   res.send({});
+    if(existingUser)
+    {
+       throw new BadRequestError('Email in use');
+    }
+    const user = User.build({ email, password });
+    await user.save();
+    
+    res.status(201).send(user);    
+  
    
 });
 
