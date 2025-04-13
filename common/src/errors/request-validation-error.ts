aac4fb6a1ceb9919b506577c5774c502d0128424
Anju Alexander <75101813+Anju-Alexander@users.noreply.github.com>
2025-04-13 19:07:07 +0530

    Updates

--- common/src/errors/request-validation-error.ts
+++ common/src/errors/request-validation-error.ts
@@ -1,10 +1,19 @@
-import { ValidationError } from 'express-validator';
+//import { ValidationError } from 'express-validator';
 import { CustomError } from './custom-error';
+
+type ValidationErr = {
+  msg: string;
+  param: string;
+  location: string;
+  value?: any;
+  type?: string;
+  path?: string; // some versions use `path` instead of `param`
+};
 
 export class RequestValidationError extends CustomError {
   statusCode = 400;
 
-  constructor(public errors: ValidationError[]) {
+  constructor(public errors: ValidationErr[]) {
     super('Invalid request parameters');
 
     // Only because we are extending a built in class
