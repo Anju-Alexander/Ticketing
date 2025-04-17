25da56b2d360209d537885f54b4e01a1500f4a06
Anju Alexander <75101813+Anju-Alexander@users.noreply.github.com>
2025-04-17 23:35:50 +0530

    nats-listener

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,24 +1,36 @@
 import mongoose from 'mongoose';
+
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
-
-// Connect to Mongo and start the server
 const start = async () => {
-    if(!process.env.JWT_KEY){
-        throw new Error('JWT Key must be defined');
-    }
-    if(!process.env.MONGO_URI){
-      throw new Error('MONGO_URI Key must be defined');
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
+
   try {
+    await natsWrapper.connect('ticketing', 'alsdkj', 'http://nats-srv:4222');
+
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
+
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('Connected to Tickets MongoDB');
+    console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
   }
 
   app.listen(3000, () => {
-    console.log('Listening on port 3000!!!!');
+    console.log('Listening on port 3000!!!!!!!!');
   });
 };
 
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,9 @@ import express, { Request, Response } from 'express';
 
 import { requireAuth, validateRequest } from '@anju102/common';
 import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
+
 
 const { body } = require('express-validator');
 
@@ -28,6 +31,12 @@ router.post(
       userId: req.currentUser!.id
     });
     await ticket.save();
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.status(201).send(ticket);
   }
